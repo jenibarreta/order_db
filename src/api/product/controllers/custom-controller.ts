9a36b23e72a0160cae908a5b4be222a071a40259
Tobii0822 <panganibankennedy08@gmail.com>
2026-06-11 10:39:13 +0800

    third commit

--- src/api/product/controllers/custom-controller.ts
+++ src/api/product/controllers/custom-controller.ts
@@ -1,4 +1,50 @@
 export default {
 
-    
+    async getProducts(ctx) {
+        try {
+            const data = await strapi.documents('api::product.product').findMany()
+            ctx.body = data;
+            ctx.status = 200;
+        } catch (err) {
+            ctx.badRequest(err)
+        }
+    },
+
+    async getLowStocks(ctx) {
+        try {
+            const data = await strapi.documents('api::product.product').findMany({
+                filters: {
+                    stock: {
+                        $lte: 10
+                    }
+                }
+            })
+            ctx.body = data;
+            ctx.status = 200;
+        } catch (err) {
+            ctx.badRequest(err)
+        }
+    },
+
+    async getLowStocksByPriceRange(ctx) {
+        try {
+            const data = await strapi.documents('api::product.product').findMany({
+                filters: {
+                    stock: {
+                        $lte: 10
+                    },
+                    price: {
+                        $gte: 500,
+                        $lte: 1500,
+                    }
+                }
+            });
+
+            ctx.body = data;
+            ctx.status = 200;
+        } catch (err) {
+            ctx.badRequest(err);
+        }
+    }
+
 }
--- src/api/product/routes/custom-routes.ts
+++ src/api/product/routes/custom-routes.ts
@@ -1,3 +1,21 @@
 export default {
-    routes:[]
+    routes: [
+        {
+            method: 'GET',
+            path: '/products/get-all-products',
+            handler: 'custom-controller.getProducts'
+        },
+
+        {
+            method: 'GET',
+            path: '/products/get-low-stocks',
+            handler: 'custom-controller.getLowStocks'
+        },
+
+        {
+            method: 'GET',
+            path: '/products/get-low-stocks-price-range',
+            handler: 'custom-controller.getLowStocksByPriceRange'
+        }
+    ]
 }
